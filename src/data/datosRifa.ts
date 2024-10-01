75248687845ad82cf4c8bc480f55a593e7dbe7dc
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2024-10-01 13:49:22 -0300

    Lo basico para empezar

---
+++ src/data/datosRifa.ts
@@ -0,0 +1,81 @@
+import asdjson from '../../numeros-melonie.json'
+
+interface Comprador {
+    nombre: string;
+    instagram: string;
+}
+
+interface Numeros {
+    numero: number;
+    comprador: Comprador;
+    comprado: boolean;
+    ganador: boolean;
+}
+
+interface Premios {
+    premio: string;
+    cantidad: number;
+    descripcion: string;
+    donador: string;
+}
+
+export const premiosMelonie: Premios[] = [
+    {
+        premio: "Landing Page",
+        cantidad: 1,
+        descripcion: "Creacion de pagina web estatica o 'informativa' personalizada, la cual puede ser transferible y para cuando lo necesite",
+        donador: "Rubik Apps"
+    },{
+        premio: "2 Vinos",
+        cantidad: 3,
+        descripcion: "3 Premios de dos vinos cada uno para quedar eposh",
+        donador: "Tito"
+    },{
+        premio: "Six pack cervezas",
+        cantidad: 1,
+        descripcion: "Six pack de cervezas para pasar la se",
+        donador: "Sujey"
+    },{
+        premio: "Frazadas de 2 plazas",
+        cantidad: 2,
+        descripcion: "Frazadas hechas en la legendaria fabrica Mincha Sur",
+        donador: ""
+    },{
+        premio: "Freestyle lateral o cintillo de trenzas",
+        cantidad: 1,
+        descripcion: "",
+        donador: "Sujey"
+    },{
+        premio: "Kuchen familiar de Nuez",
+        cantidad: 1,
+        descripcion: "Rico Kuchen fresquito hecho con mucho amor",
+        donador: "Hornearte"
+    },{
+        premio: "Blanqueamiento dental",
+        cantidad: 1,
+        descripcion: "Sesion de blanqueamiento dental, el cual consiste en ir al lab, crean cubetas de sus dientes y entregaran jeringas para blanquear en casa",
+        donador: "Chumi"
+    },{
+        premio: "Sesion de masoterapia",
+        cantidad: 1,
+        descripcion: "Sesion de masoterapia con Carlos Gonzales (Con requisitos y condiciones)",
+        donador: "Carlos Gonzales"
+    },{
+        premio: "Bolsa con dulces",
+        cantidad: 1,
+        descripcion: "Bolsa con variados dulces, aprox 15k en dulces",
+        donador: "Vale"
+    },{
+        premio: "Sesion de masoterapia",
+        cantidad: 1,
+        descripcion: "Una sesion de 40 minutos de masajes descontracturantes o relajantes, con o sin ventosas",
+        donador: "Francisca"
+    },{
+        premio: "Conjunto de Aros y Cadena",
+        cantidad: 1,
+        descripcion: "Conjunto de aros y cadena de plata",
+        donador: "Macarena"
+    }
+]
+
+export const numerosTabla: Numeros[] = asdjson;
