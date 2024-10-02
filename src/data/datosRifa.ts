f13ca7bcd9e092c45da9d3c3a2a336b6677cf9a8
mikish_ash@hotmail.com <mikish_ash@hotmail.com>
2024-10-01 21:33:01 -0300

    Corregido el espaciado de las descripciones y tambien los typos

--- src/data/datosRifa.ts
+++ src/data/datosRifa.ts
@@ -23,7 +23,7 @@ export const premiosMelonie: Premios[] = [
     {
         premio: "Landing Page",
         cantidad: 1,
-        descripcion: "Creacion de pagina web estatica o 'informativa' personalizada, la cual puede ser transferible y para cuando lo necesite",
+        descripcion: "Creación de página web estática o 'informativa' personalizada, la cual puede ser transferible y para cuando lo necesite.",
         donador: "Rubik Apps"
     },{
         premio: "2 Vinos",
@@ -38,27 +38,27 @@ export const premiosMelonie: Premios[] = [
     },{
         premio: "Frazadas de 2 plazas",
         cantidad: 2,
-        descripcion: "Frazadas hechas en la legendaria fabrica Mincha Sur",
+        descripcion: "Frazadas hechas en la legendaria fábrica Mincha Sur",
         donador: "Mincha Sur"
     },{
         premio: "Freestyle lateral o cintillo de trenzas",
         cantidad: 1,
-        descripcion: "Las trenzas mas sarpadas de lo hermida",
+        descripcion: "Las trenzas más sarpadas de lo hermida",
         donador: "Sujey"
     },{
         premio: "Kuchen familiar de Nuez",
         cantidad: 1,
-        descripcion: "Rico Kuchen fresquito hecho con mucho amor",
+        descripcion: "Rico kuchen fresquito, hecho con mucho amor",
         donador: "Hornearte"
     },{
         premio: "Blanqueamiento dental",
         cantidad: 1,
-        descripcion: "Sesion de blanqueamiento dental, el cual consiste en ir al lab, crean cubetas de sus dientes y entregaran jeringas para blanquear en casa",
+        descripcion: "Sesión de blanqueamiento dental, el cual consiste en ir al laboratorio, crearán cubetas de sus dientes y entregarán jeringas para blanquear en casa",
         donador: "Chumi"
     },{
-        premio: "Sesion de masoterapia",
+        premio: "Sesión de masoterapia",
         cantidad: 1,
-        descripcion: "Sesion de masoterapia con Carlos Gonzales (Con requisitos y condiciones)",
+        descripcion: "Sesión de masoterapia con Carlos Gonzales (Con requisitos y condiciones)",
         donador: "Carlos Gonzales"
     },{
         premio: "Bolsa con dulces",
@@ -66,9 +66,9 @@ export const premiosMelonie: Premios[] = [
         descripcion: "Bolsa con variados dulces, aprox 15k en dulces",
         donador: "Vale"
     },{
-        premio: "Sesion de masoterapia",
+        premio: "Sesión de masoterapia",
         cantidad: 1,
-        descripcion: "Una sesion de 40 minutos de masajes descontracturantes o relajantes, con o sin ventosas",
+        descripcion: "Una sesión de 40 minutos de masajes descontracturantes o relajantes, con o sin ventosas",
         donador: "Francisca Letelier"
     },{
         premio: "Conjunto de Aros y Cadena",
